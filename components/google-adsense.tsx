580bbc3f73b0c385c1108fc6ae6458e15fc5a00e
ZKFKSXK <karanta.labs@gmail.com>
2025-12-02 14:08:02 +0000

    feat: 키 변경

--- components/google-adsense.tsx
+++ components/google-adsense.tsx
@@ -7,9 +7,9 @@ export const GoogleAdSense = () => {
   return (
     <Script
       async
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${process.env.NEXT_PUBLIC_GAPID}`}
+      src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3981909285877075'
       crossOrigin='anonymous'
-      strategy='lazyOnload'
+      strategy='afterInteractive'
     />
   );
 };
